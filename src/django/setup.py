473c89751104fa58f797fc4d7e9b57611058275c
Breno Silva <brenokcc@yahoo.com.br>
2025-08-21 09:55:05 -0300

    0.6.1

--- src/django/setup.py
+++ src/django/setup.py
@@ -4,7 +4,7 @@ install_requires = []
 
 setup(
     name='slthcore',
-    version='0.5.9',
+    version='0.6.1',
     packages=find_packages(),
     install_requires=install_requires,
     include_package_data=True,
